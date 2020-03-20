7a39e15accaee0f9ed75008e25e138eea1851811
Aminata <61282231+TraoreAminata99@users.noreply.github.com>
2020-03-20 16:07:17 +0000

    Add files via upload

---
+++ Appointment.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Truman
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Appointment : ContentPage
+    {
+        public Appointment()
+        {
+            InitializeComponent();
+            btn2.Clicked += Btn2_Clicked;
+        }
+
+       async private void Btn2_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Confirm());
+
+        }
+    }
+}
